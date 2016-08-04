a5c889b7b6d51cd4cb0bb9c6b88ade84470f7813
meghanamurthysn <meghanamurthy@berkeley.edu>
2016-08-05 01:33:52 +0530

    HW03_ex06_v01

--- HW03_ex06.py
+++ HW03_ex06.py
@@ -8,10 +8,13 @@
 # See 6.1: "write a compare function takes two values, x and y, and returns 1
 # if x > y, 0 if x == y, and -1 if x < y."
 # When you submit only include your final function: compare
-
-
-
-
+def compare(x, y):
+    if x>y:
+        return 1
+    elif x==y:
+        return 0
+    else:
+        return -1
 
 ###############################################################################
 # Exercise 6.2
@@ -21,20 +24,21 @@
 # When you submit only include your final function: hypotenuse
 # Do develop incrementally. Do not share in your final push your incremental
 # work.
-
-
-
-
+import math
+def hypotenuse(leg1, leg2):
+    hyp=leg1**2+leg2**2
+    return math.sqrt(hyp)
 
 ###############################################################################
 # Exercise 6.4
 # See 6.4: "write a function is_between(x, y, z) that returns True if x ≤ y ≤ z
 # or False otherwise"
 # When you submit only include your final function: is_between
-
-
-
-
+def is_between(x, y, z):
+    if x<=y<=z:
+        return True
+    else:
+        return False
 
 ###############################################################################
 # Exercise 3.2
@@ -43,10 +47,10 @@
 # Remember that you can use the built-in function len to check the length of a
 # string."
 # When you submit only include your final function: is_palindrome
-
-
-
-
+def is_palindrome(s):
+    if s==s[::-1]:
+        return True
+    return False
 
 ###############################################################################
 # Exercise 4
@@ -57,11 +61,15 @@
 # Note: Please use the provided definition and only plan for positive integers
 # (whole numbers not including zero)
 # When you submit only include your final function: is_power
-
-
-
-
-
+import math
+def is_power(a, b):
+    if a==b or a==1:
+        return True
+    elif a%b==0 and is_power(a/b,b):
+        return True
+    else:
+        return False
+    
 ###############################################################################
 def main():
     """Your functions will be called within this function."""
@@ -77,28 +85,28 @@ def main():
     ###########################################################################
     # # Uncomment the below to test and before commiting:
     # # Exercise 1
-    # print(compare(1, 1))
-    # print(compare(1, 2))
-    # print(compare(2, 1))
+    print(compare(1, 1))
+    print(compare(1, 2))
+    print(compare(2, 1))
     # # # Exercise 2
-    # print(hypotenuse(1, 1))
-    # print(hypotenuse(3, 4))
-    # print(hypotenuse(1.2, 12))
+    print(hypotenuse(1, 1))
+    print(hypotenuse(3, 4))
+    print(hypotenuse(1.2, 12))
     # # # Exercise 3
-    # print(is_between(1, 2, 3))
-    # print(is_between(2, 1, 3))
-    # print(is_between(3, 1, 2))
-    # print(is_between(1, 1, 2))
+    print(is_between(1, 2, 3))
+    print(is_between(2, 1, 3))
+    print(is_between(3, 1, 2))
+    print(is_between(1, 1, 2))
     # # # Exercise 6
-    # print(is_palindrome("Python"))
-    # print(is_palindrome("evitative"))
-    # print(is_palindrome("sememes"))
-    # print(is_palindrome("oooooooooooo"))
+    print(is_palindrome("Python"))
+    print(is_palindrome("evitative"))
+    print(is_palindrome("sememes"))
+    print(is_palindrome("oooooooooooo"))
     # # # Exercise 7
-    # print(is_power(28, 3))
-    # print(is_power(27, 3))
-    # print(is_power(248832, 12))
-    # print(is_power(248844, 12))
+    print(is_power(28, 3))
+    print(is_power(27, 3))
+    print(is_power(248832, 12))
+    print(is_power(248844, 12))
 
 
 if __name__ == "__main__":
